352e2138c75dd731d41d46133628eca092a9f730
Tresa Horney <tresa.e@gmail.com>
2014-05-23 14:51:26 -0700

    updated filters with parseEntry and deleted duplicate functions in controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,22 @@ angular.module('myApp.controllers', [])
 	.controller('FrameController', ['$scope', function($scope){
 		$scope.today = new Date(); //Today's date
 		$scope.name = "Tresa Horney";
-}])
-	.controller('DashboardController', ['$scope', function ($scope) {
-		// body...
+	}])
+	.controller('DashboardController', ['$scope',function ($scope) {
+		var date = new Date();
+		$scope.dateFilters = {
+			'all': 'all',
+			'next week': new Date(date.setDate(date.getDate() +7)),
+			'tomorrow': chrono.parseDate('tomorrow at 11:59pm'),
+			'today': chrono.parseDate('today at 11:59pm')
+			}
+		$scope.excludeByDate = function(input) {
+			if($scope.keepDate === 'all') {
+				return true;
+			} else {
+				return new Date(input.start.dateTime).getTime() < 
+					$scope.keepDate.getTime();
+			}
+		}
 	}]);
+
